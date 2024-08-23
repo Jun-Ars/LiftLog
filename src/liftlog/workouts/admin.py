ffcb1a11ec04831f4ec04cb872609515729d3182
Junaid Arshad <jun.arshad@gmail.com>
2024-08-23 15:53:46 -0400

    Register workout models in admin.py

--- src/liftlog/workouts/admin.py
+++ src/liftlog/workouts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models.exercise import Exercise
+from .models.set import Set
+from .models.workout import Workout
 
 # Register your models here.
+admin.site.register(Exercise)
+admin.site.register(Set)
+admin.site.register(Workout)
